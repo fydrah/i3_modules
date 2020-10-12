207327ddcf9fc0c9e65a1106737bf46e160dec2e
Flavien Hardy <flav.hardy@gmail.com>
2020-10-12 14:16:09 -0400

    add gcloud current project

--- gcloud.py
+++ gcloud.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
+import configparser
 
 class Py3status:
 
     def gcloudcontext(self):
-        GCLOUDCONFIG = os.getenv('HOME') + "/.config/gcloud/active_config"
+        GCLOUDCONFIG_DIR = os.getenv('HOME') + '/.config/gcloud'
+        GCLOUDCONFIG = GCLOUDCONFIG_DIR + '/active_config'
         f = open(GCLOUDCONFIG, 'r')
-        GCLOUDACTIVECONFIG=f.read()
+        gcloud_activeconfig=f.read()
         f.close()
+        gcloud_config = configparser.ConfigParser()
+        gcloud_config.read(GCLOUDCONFIG_DIR + '/configurations/config_' + gcloud_activeconfig)
+        gcloud_project = ''
+        try:
+            gcloud_project = ':' + gcloud_config['core']['project']
+        except KeyError:
+            pass
         return {
-            'full_text': 'gcloud:' + GCLOUDACTIVECONFIG,
+            'full_text': 'gcloud:' + gcloud_activeconfig + gcloud_project,
             'cached_until': self.py3.time_in(seconds=5)
         }
 
